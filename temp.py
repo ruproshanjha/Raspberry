2d5ac2f1662067b1cdf60c00be082f20ae10df31
Rup Roshan Jha <125679654+ruproshanjha@users.noreply.github.com>
2025-11-20 02:02:02 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,7 @@
-python3 - << 'EOF'
 import cv2
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-print("Opened:", cap.isOpened())
-EOF
+
+for i in range(0, 32):
+    cap = cv2.VideoCapture(i, cv2.CAP_V4L2)
+    ok, frame = cap.read()
+    print(f"Video{i} -> opened={cap.isOpened()}, frame={ok}")
+    cap.release()
